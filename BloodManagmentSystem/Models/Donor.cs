6be831db3c1dae1c4623cab97ec25e8a29d8d868
Marta Sajdak <crappylime@gmail.com>
2017-09-13 15:28:46 +0200

    Create Donor class

---
+++ BloodManagmentSystem/Models/Donor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BloodManagmentSystem.Models
+{
+    public class Donor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public BloodType BloodType { get; set; }
+    }
+}
