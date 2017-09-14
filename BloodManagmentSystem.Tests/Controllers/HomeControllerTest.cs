0bd0172339ad0d0ec1e1eeb0abc61c87e5ed77f8
Cezary Broś <cezary.bros+git@gmail.com>
2017-09-14 15:59:24 +0200

    Update references

--- BloodManagmentSystem.Tests/Controllers/HomeControllerTest.cs
+++ BloodManagmentSystem.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using BloodManagmentSystem;
-using BloodManagmentSystem.Controllers;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BloodManagmentSystem.Tests.Controllers
 {
@@ -20,32 +13,6 @@ namespace BloodManagmentSystem.Tests.Controllers
 
             // Act
             ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
