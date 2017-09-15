cf94024b5f146c1181cb2b51a43406d790e7a0a0
Cezary Broś <cezary.bros+git@gmail.com>
2017-09-15 20:17:28 +0200

    Add email request form view

--- BloodManagmentSystem/Services/MyEmailService.cs
+++ BloodManagmentSystem/Services/MyEmailService.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using System.Net.Mail;
+﻿using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace BloodManagmentSystem.Services
@@ -8,17 +7,8 @@ namespace BloodManagmentSystem.Services
     {
         public async Task<bool> SendEmailAsync(string modelTo, string modelMessage, string modelSubject)
         {
-            var from = new MailAddress("cbmlody@gmail.com");
+            var from = new MailAddress("bms.mailingservice@gmail.com");
             var to = new MailAddress(modelTo);
-
-            const string host = "smtp.gmail.com";
-
-            const string userName = "cbmlody@gmail.com";
-            const string password = "Gr4phicu$";
-            bool.TryParse("true", out var enableSsl);
-            bool.TryParse("false", out var useDefaultCredentials);
-
-            int.TryParse("587", out var port);
 
             using (var mail = new MailMessage(from, to))
             {
@@ -28,17 +18,6 @@ namespace BloodManagmentSystem.Services
 
                 using (var client = new SmtpClient())
                 {
-                    client.Host = host;
-                    client.EnableSsl = enableSsl;
-                    client.Port = port;
-                    client.UseDefaultCredentials = useDefaultCredentials;
-
-                    if (!client.UseDefaultCredentials && !string.IsNullOrEmpty(userName) &&
-                        !string.IsNullOrEmpty(password))
-                    {
-                        client.Credentials = new NetworkCredential(userName, password);
-                    }
-
                     await client.SendMailAsync(mail);
                 }
             }
--- BloodManagmentSystem/ViewModel/EmailFormModel.cs
+++ BloodManagmentSystem/ViewModel/EmailFormModel.cs
@@ -1,9 +1,13 @@
-﻿namespace BloodManagmentSystem.ViewModel
+﻿using System.Web.Mvc;
+
+namespace BloodManagmentSystem.ViewModel
 {
     public class EmailFormModel
     {
         public string To { get; set; }
         public string Subject { get; set; }
+
+        [AllowHtml]
         public string Message { get; set; }
     }
 }
