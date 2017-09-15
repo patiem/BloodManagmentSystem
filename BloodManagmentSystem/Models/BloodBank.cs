8c6af3129d53cc9a31088c406adc66e0ed799f76
pp.mikulska <pp.mikulska@gmail.com>
2017-09-15 10:30:03 +0200

    Add required and stringlenghth annotations for bank and request

--- BloodManagmentSystem/Models/BloodBank.cs
+++ BloodManagmentSystem/Models/BloodBank.cs
@@ -1,9 +1,17 @@
-﻿namespace BloodManagmentSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloodManagmentSystem.Models
 {
     public class BloodBank
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string City { get; set; }
     }
 }
